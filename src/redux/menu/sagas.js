6dde4933f2a0dd5fec2884a613814e9f1c731d71
Egor Ptashnik <jahorup@gmail.com>
2020-12-04 15:02:13 +0300

    Remove unused selector

--- src/redux/menu/sagas.js
+++ src/redux/menu/sagas.js
@@ -1,10 +1,8 @@
 import { all, put, call, select } from 'redux-saga/effects';
 import getMenuData from 'services/menu';
-import getRole from './selecotrs';
 
 export function* GET_DATA() {
   const menuData = yield call(getMenuData);
-  const role = yield select(getRole);
 
   yield put({
     type: 'menu/SET_STATE',
