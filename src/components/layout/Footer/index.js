decb85793e0542bea14a1a02493dcdaf01822e29
Alexei Mairyn <alexei.mairyn@gmail.com>
2020-12-05 18:10:36 +0300

    fix: remove unsused

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import classNames from 'classnames'
-import style from './style.module.scss'
+import React from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
+import style from './style.module.scss';
 
-const mapStateToProps = ({ settings }) => ({ settings })
+const mapStateToProps = ({ settings }) => ({ settings });
 
 @connect(mapStateToProps)
 class Footer extends React.Component {
   render() {
     const {
       settings: { isContentNoMaxWidth },
-    } = this.props
+    } = this.props;
     return (
       <div
         className={classNames(style.footer, {
@@ -21,13 +21,16 @@ class Footer extends React.Component {
           <div className="row">
             <div className="col-md-8">
               <p>
-                <strong>Air UI Admin Template - Best Solution for Your Next Big App!</strong>
+                <strong>
+                  Air UI Admin Template - Best Solution for Your Next Big App!
+                </strong>
               </p>
               <p>
-                Air UI is a set of modern professional Html / React / Vue and Angular based
-                templates. This is a powerful and super flexible tool, which suits best for any kind
-                of web application: Web Sites, Web Applications, Hybrid Apps, CRM, CMS, Admin
-                Panels, etc.
+                Air UI is a set of modern professional Html / React / Vue and
+                Angular based templates. This is a powerful and super flexible
+                tool, which suits best for any kind of web application: Web
+                Sites, Web Applications, Hybrid Apps, CRM, CMS, Admin Panels,
+                etc.
               </p>
               <p>&copy; 2019 Mediatec Software</p>
             </div>
@@ -41,8 +44,8 @@ class Footer extends React.Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
