64d215ff40ecdec400b9208f5cf9cca3dd6afc94
Catherine Skakun <skakun.studio@gmail.com>
2021-01-19 03:35:38 +0700

    restore hijack on page reload

--- src/redux/hijack/actions.js
+++ src/redux/hijack/actions.js
@@ -3,6 +3,7 @@ const actions = {
   FETCH_CREDENTIALS_SUCCESS: 'hijack/FETCH_CREDENTIALS_SUCCESS',
   FETCH_CREDENTIALS_FAILURE: 'hijack/FETCH_CREDENTIALS_FAILURE',
   LOGOUT: 'hijack/LOGOUT',
+  RESTORE: 'hijack/RESTORE',
 };
 
 export default actions;
