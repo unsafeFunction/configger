5e47611cbe5933f4533ba018a199496287f8d0fe
Catherine Skakun <skakun.studio@gmail.com>
2021-02-12 23:14:13 +0700

    fix: initialize rackboard

--- src/components/widgets/Rackboard/index.jsx
+++ src/components/widgets/Rackboard/index.jsx
@@ -4,6 +4,7 @@ import actions from 'redux/scanSessions/actions';
 import { Table, Button, Popover, Input, Popconfirm, Tag } from 'antd';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { constants } from 'utils/constants';
 import styles from './styles.module.scss';
 
 const Rackboard = ({ rackboard, scanId }) => {
@@ -11,24 +12,40 @@ const Rackboard = ({ rackboard, scanId }) => {
   const [currentTubeID, setCurrentTubeID] = useState('');
   const [popoverVisible, setPopoverVisible] = useState(null);
 
-  const handleSave = useCallback((record) => {
+  const initialRackboard = [...Array(6).keys()].map(i => ({
+    letter: String.fromCharCode(constants?.A + i),
+    col1: { tube_id: null, status: 'empty' },
+    col2: { tube_id: null, status: 'empty' },
+    col3: { tube_id: null, status: 'empty' },
+    col4: { tube_id: null, status: 'empty' },
+    col5: { tube_id: null, status: 'empty' },
+    col6: { tube_id: null, status: 'empty' },
+    col7: { tube_id: null, status: 'empty' },
+    col8: { tube_id: null, status: 'empty' },
+  }));
+
+  const handleSave = useCallback(
+    record => {
       dispatch({
         type: actions.UPDATE_TUBE_REQUEST,
-        payload: {record, tube_id: currentTubeID},
+        payload: { record, tube_id: currentTubeID },
       });
     },
     [dispatch, currentTubeID],
   );
 
-  const handleDelete = useCallback((record) => {
-    dispatch({
-      type: actions.DELETE_TUBE_REQUEST,
-      payload: {record, scanId}
-    });
-    setPopoverVisible(null);
-  }, [scanId]);
+  const handleDelete = useCallback(
+    record => {
+      dispatch({
+        type: actions.DELETE_TUBE_REQUEST,
+        payload: { record, scanId },
+      });
+      setPopoverVisible(null);
+    },
+    [scanId],
+  );
 
-  const handleChangeTubeID = useCallback((e) => {
+  const handleChangeTubeID = useCallback(e => {
     setCurrentTubeID(e.target.value);
   }, []);
 
@@ -69,7 +86,11 @@ const Rackboard = ({ rackboard, scanId }) => {
                   cancelText="No"
                   onConfirm={() => handleSave(record?.[`col${i + 1}`])}
                 >
-                  <Button disabled={!currentTubeID} className={styles.popoverBtn} type="primary">
+                  <Button
+                    disabled={!currentTubeID}
+                    className={styles.popoverBtn}
+                    type="primary"
+                  >
                     Save
                   </Button>
                 </Popconfirm>
@@ -80,7 +101,7 @@ const Rackboard = ({ rackboard, scanId }) => {
                   cancelText="No"
                   onConfirm={() => handleDelete(record?.[`col${i + 1}`])}
                 >
-                  <Button className={styles.popoverBtn}  danger>
+                  <Button className={styles.popoverBtn} danger>
                     Delete
                   </Button>
                 </Popconfirm>
@@ -92,15 +113,22 @@ const Rackboard = ({ rackboard, scanId }) => {
                   cancelText="No"
                   // onConfirm={}
                 >
-                  <Button className={styles.popoverBtn} disabled >Invalidate</Button>
+                  <Button className={styles.popoverBtn} disabled>
+                    Invalidate
+                  </Button>
                 </Popconfirm>
 
-                <Button onClick={handleClosePopover} className={styles.popoverBtn}>Cancel</Button>
+                <Button
+                  onClick={handleClosePopover}
+                  className={styles.popoverBtn}
+                >
+                  Cancel
+                </Button>
               </div>
             }
             trigger="click"
             onVisibleChange={() => {
-              setPopoverVisible(record?.[`col${i + 1}`]?.id)
+              setPopoverVisible(record?.[`col${i + 1}`]?.id);
               setCurrentTubeID(record?.[`col${i + 1}`]?.tube_id);
             }}
           >
@@ -141,7 +169,7 @@ const Rackboard = ({ rackboard, scanId }) => {
     <>
       <Table
         columns={columns}
-        dataSource={rackboard?.items}
+        dataSource={rackboard?.items || initialRackboard}
         loading={rackboard?.isLoading}
         pagination={false}
         scroll={{ x: 'max-content' }}
