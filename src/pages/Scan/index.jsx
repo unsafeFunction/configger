8a215950f9cce6481598834b915f5c5e9df1c12e
ilya lisovski <ilya.lisovski@yandex.ru>
2021-02-17 21:23:45 +0300

    fix: scan another navigation action

--- src/pages/Scan/index.jsx
+++ src/pages/Scan/index.jsx
@@ -71,8 +71,16 @@ const Scan = () => {
           id: scan?.id,
         },
       });
+
+      if (currentScanOrder >= scansTotal - 1) {
+        history.push({ search: `?scanOrder=0` });
+        setCurrentScanOrder(0);
+      } else {
+        history.push({ search: `?scanOrder=${currentScanOrder + 1}` });
+        setCurrentScanOrder(currentScanOrder + 1);
+      }
     },
-    [dispatch, scan],
+    [dispatch, scan, scansTotal, currentScanOrder],
   );
 
   const handleVoidScan = useCallback(() => {
