e32a9638bf704d098a3b11db7fed00f5f1053c27
Aliaxei Mairyn <mairyn.dev@gmail.com>
2021-07-12 20:55:47 +0000

    Update errorOutput.js

--- src/utils/errorOutput.js
+++ src/utils/errorOutput.js
@@ -1,6 +1,7 @@
 const errorOutput = (error) => {
   if (error?.response?.data.field_errors) {
     const err = error.response.data.field_errors;
+    // This split array of erros to separate and join them to string
     return JSON.stringify(err, null, 2).replace(/{|}|"|,/g, '');
   }
   if (error?.response?.data.detail) {
