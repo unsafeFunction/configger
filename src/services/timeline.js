86da8b8fffb78b770e5a92f9d491a0d07b89ae84
Kate Skakun <skakun.studio@gmail.com>
2021-12-14 17:05:49 +0700

    fix: trailing slash to timeline request

--- src/services/timeline.js
+++ src/services/timeline.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/prefer-default-export */
 import axiosClient from 'utils/axiosClient';
 
 export const loadTimeline = async (id) => {
   try {
-    const timeline = await axiosClient.get(`/runs/${id}/timeline`);
+    const timeline = await axiosClient.get(`/runs/${id}/timeline/`);
     return timeline;
   } catch (error) {
     return error;
