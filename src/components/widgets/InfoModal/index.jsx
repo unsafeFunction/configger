bea26e898740d2c1e8e3570e48b96fc3c131508f
Alexei Mairyn <alexei.mairyn@gmail.com>
2022-02-04 01:14:09 +0300

    feat: tooltip improvements: texts, icon, display of tooltip

--- src/components/widgets/InfoModal/index.jsx
+++ src/components/widgets/InfoModal/index.jsx
@@ -9,32 +9,39 @@ const useTip = (type) => {
         <>
           <div>
             <b className={styles.modalTitle}>Save session</b>
+            <p className={styles.tipDescription}>
+              <p>
+                1.Saves all scans that have the status “Completed” in the
+                history table then ends the session.
+              </p>
+              <p>
+                2.Save the session once you are done scanning all samples for
+                one company, including rejected samples if there are any.
+              </p>
+            </p>
             <img
               className={styles.image}
               src="https://qa.media.lims.mirimus.com/save-session.gif"
               alt="save-session"
             />
-            <p className={styles.tipDescription}>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, Lorem Ipsum
-            </p>
           </div>
           <div>
             <b className={styles.modalTitle}>Cancel session</b>
+            <p className={styles.tipDescription}>
+              <p>
+                1. Permanently deletes all scans in the session then ends the
+                session.
+              </p>
+              <p>
+                2.Cancel the session if you don’t plan to scan samples or if you
+                want to start over.
+              </p>
+            </p>
             <img
               className={styles.image}
               src="https://qa.media.lims.mirimus.com/close-session.gif"
               alt="cancel-session"
             />
-            <p className={styles.tipDescription}>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic
-              typesetting.
-            </p>
           </div>
         </>
       );
@@ -43,17 +50,15 @@ const useTip = (type) => {
       return (
         <div>
           <b className={styles.modalTitle}>Void scan</b>
+          <p className={styles.tipDescription}>
+            Permanently deletes the scan that is currently displayed on the scan
+            table.
+          </p>
           <img
             className={styles.image}
             src="https://qa.media.lims.mirimus.com/void-scan.gif"
             alt="void-scan"
           />
-          <p className={styles.tipDescription}>
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, Lorem Ipsum
-          </p>
         </div>
       );
     }
@@ -61,17 +66,18 @@ const useTip = (type) => {
       return (
         <div>
           <b className={styles.modalTitle}>Save scan</b>
+          <p className={styles.tipDescription}>
+            <p>
+              1.Saves all samples displayed on the scan table and associates the
+              scan with a pool name.
+            </p>
+            <p>2.Once a scan is saved, the scan cannot be edited.</p>
+          </p>
           <img
             className={styles.image}
             src="https://qa.media.lims.mirimus.com/save-scan.gif"
             alt="void-scan"
           />
-          <p className={styles.tipDescription}>
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, Lorem Ipsum
-          </p>
         </div>
       );
     }
