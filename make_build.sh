4ff92e10070016de82a73d413a2da67463776cb2
rostislav.malyshev <rostislav.malyshev@eloquentbits.com>
2022-05-03 05:42:33 -0400

    Изменил(а) на 'make_build.sh'

--- make_build.sh
+++ make_build.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-#  Test local build
-cd /srv/Projects/lims-frontend
+#  Test build
+
+if [[ $1 == "local" ][
+then
+        rclone_conf="bunny_rm:"
+elif [[ $1 == "mirimus-qa:" ]]
+then
+        rclone_conf="bunny_qa"
+elif [[ $1 == "mirimus-prod:" ]]
+then
+        rclone_conf="bunny_prod"
+elif [[ $1 == "accelevir-prod:" ]]
+then
+        rclone_conf="bunny_aclv:"
+elif [[ $1 == "wynn-prod" ]]
+then
+        rclone_conf="bunny_wynn:"
+else
+        echo "Wrong build conf"
+        exit 1
+fi;
+echo "Building:"$1
+frontend_dir="/srv/Projects/lims-frontend"
+frontend_build_dir="/srv/Projects/lims-frontend/build"
+cd  $frontend_dir
 echo "git stash"
 git stash
 echo  "git pull"
-git pull https://rostislav.malyshev:"2460226Rr!"@qa.git.mirimus.com/Mirimus/lims-frontend.git
-echo "npm run build:local"
-npm run build:local
-echo "push to bunny" 
-
+git pull
+cp template.build .env-cmdrc
+echo "npm run build:"$1
+npm run build:$1
+echo "push to bunny"
+#rclone sync $frontend_build_dir $rclone_conf 
