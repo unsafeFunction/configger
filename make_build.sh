316a2bccf25144aba9d78eb6cbeb394323ac3ed8
rostislav.malyshev <rostislav.malyshev@eloquentbits.com>
2022-05-03 06:10:23 -0400

    Изменил(а) на 'make_build.sh'

--- make_build.sh
+++ make_build.sh
@@ -1,26 +1,31 @@
-#!/bin/bash
+  GNU nano 6.2                                           make_build.sh                                                    #!/bin/bash
 #  Test build
 
 if [[ $1 == "local" ]]
 then
         rclone_conf="bunny_rm:"
+		build_script=$1
 elif [[ $1 == "mirimus-qa" ]]
 then
         rclone_conf="bunny_qa:"
+		build_script=$1
 elif [[ $1 == "mirimus-prod" ]]
 then
         rclone_conf="bunny_prod:"
+		build_script=$1
 elif [[ $1 == "accelevir-prod" ]]
 then
-        rclone_conf="bunny__aclv:"
+        rclone_conf="bunny_aclv:"
+		build_script="aclv"
 elif [[ $1 == "wynn-prod" ]]
 then
         rclone_conf="bunny_wynn:"
+		build_script="wynn"
 else
         echo "Wrong build conf"
         exit 1
 fi;
-echo "Building:"$1
+echo "Building script:"$build_script
 frontend_dir="/srv/Projects/lims-frontend"
 frontend_build_dir="/srv/Projects/lims-frontend/build"
 cd  $frontend_dir
@@ -29,7 +34,7 @@ git stash
 echo  "git pull"
 git pull
 cp template.build .env-cmdrc
-echo "npm run build:"$1
-npm run build:$1
+echo "npm run build:"$build_script
+npm run build:$build_script
 echo "push to bunny"
 #rclone sync $frontend_build_dir $rclone_conf 
